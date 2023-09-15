25c79aeafe3b69a97e4e2fc72cb70a33fd36fedd
CaseyPotter <potterc3@mail.uc.edu>
2023-09-15 16:49:45 -0400

    finish

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       double fall;
+       double winter;
+       double spring;
+       double summer;
+       double toltal;
+
+       fall = 432.32;
+       winter = 323.23;
+       spring = 421.12;
+       summer = 433.12;
+       toltal = summer + spring + fall + winter;
+
+        System.out.println("Your toltal yearly cost is " + toltal );
+
+    }
+}
